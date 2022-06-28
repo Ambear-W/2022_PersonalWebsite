9b9657a67aaa88316b9d36a25b47ec84cd8ff9d3
Amber W <43912805+Ambear-W@users.noreply.github.com>
2022-06-27 20:16:01 -0400

    starting to make navigation bar
    - still need to figure out how to get it o appear on page
    - styles need to be done
    - setting up links --> to de done when other pages are built?

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import NavHeader from './components/Header.vue'
 </script>
 
 <template>
   <header>
+    <NavHeader />
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!!!!" />
+      <HelloWorld msg="You did it" />
     </div>
   </header>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <nav>
+      <div class="skip">
+        Skip to main content
+      </div>
+      <div class="branding">
+        <p href="">Amber Wells -</p>
+      </div>
+      <ul class="navigation">
+        <li class="nav-link" href="">About Me</li>
+        <li class="nav-link" href="">Qualifications</li>
+        <li class="nav-link" href="">Projects</li>
+        <li class="nav-link" href="">Contact me</li>
+        <li class="nav-link" href="">Resume</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style>
+</style>
